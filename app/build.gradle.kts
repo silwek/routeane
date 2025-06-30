eb5893447fd2534bae8008435b2b14859555fb86
Amandine Ferrand <amandine.silwek@gmail.com>
2025-06-30 20:12:07 +0200

    Clean default project files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.silwek.routeane"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
